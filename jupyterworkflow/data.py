3b91913a54e4ea0cc8af836e2839ad71fc607b38
Mark Biegert <mbiegert314@gmail.com>
2023-02-23 16:50:32 -0600

    Create a package

---
+++ jupyterworkflow/data.py
@@ -0,0 +1,32 @@
+import os
+from urllib.request import urlretrieve
+import pandas as pd
+
+FREMONTURL='https://data.seattle.gov/api/views/65db-xm6k/rows.csv?accessType=DOWNLOAD'
+
+def get_fremont_data(filename="Fremont.csv",url=FREMONTURL, force_download=False):
+    """
+    Download and cache fremont data
+    
+    Parameters
+    ----------
+    filename: string (optional)
+        location to save the data
+    url: string (optional)
+        web location of the data
+    force_download : bool (optional)
+        if True, force redownload of data
+        
+     -------
+     Returns
+     -------
+     data: pandas.DataFrame
+         Fremont bridge data
+    """
+    
+    
+    if force_download or not os.path.exists(filename):
+        urlretrieve(url,filename)
+    data=pd.read_csv("Fremont.csv",index_col='Date',parse_dates=True)
+    data.columns=['West','East','Total']
+    return data
