1b69a0bf3c74fdf1a35dbe8f9d8789d8dd01ec04
Mark Biegert <mbiegert314@gmail.com>
2023-02-26 08:13:49 -0600

    Added test feature. No make yet

---
+++ jupyterworkflow/tests/test_data.py
@@ -0,0 +1,8 @@
+from jupyterworkflow.data import get_fremont_data
+import pandas as pd
+import pytest
+
+def test_fremont_data():
+    data=get_fremont_data()
+    assert all(data.columns==['West', 'East', 'Total'])
+    assert isinstance(data.index,pd.DatetimeIndex)
