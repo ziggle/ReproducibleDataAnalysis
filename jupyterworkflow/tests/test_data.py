fb610ad2dc1588d877fa3b1a6a2ccf6262b26651
Mark Biegert <mbiegert314@gmail.com>
2023-02-27 19:43:53 -0600

    Bug fix example

--- jupyterworkflow/tests/test_data.py
+++ jupyterworkflow/tests/test_data.py
@@ -1,8 +1,10 @@
 from jupyterworkflow.data import get_fremont_data
 import pandas as pd
+import numpy as np
 import pytest
 
 def test_fremont_data():
     data=get_fremont_data()
     assert all(data.columns==['West', 'East', 'Total'])
     assert isinstance(data.index,pd.DatetimeIndex)
+    assert len(np.unique(data.index.time)==24)
